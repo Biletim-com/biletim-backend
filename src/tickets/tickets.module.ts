d59e9b57e3d44df048f2a56a0957ae98b3096f17
westerops <westerops@westerops-MacBook-Air.local>
2024-05-27 11:17:59 +0300

    modified biletall integration

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
-import { BiletallModule } from './biletall/biletall.module';
+import { BiletAllModule } from './biletall/biletall.module';
 
 @Module({
-  imports: [BiletallModule],
+  imports: [BiletAllModule],
   providers: [TicketsService],
   controllers: [TicketsController],
 })
