1183f86474ddebadcf4319c6e1d551823fb0c751
westerops <westerops@westerops-MacBook-Air.local>
2024-06-07 14:14:36 +0300

    added new biletall endpoints

--- src/tickets/biletall/biletall.controller.ts
+++ src/tickets/biletall/biletall.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+// src/biletall/biletall.controller.ts
+
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { BiletAllService } from './biletall.service';
-import { ScheduleListRequestDto, CompanyRequestDto } from './dto/biletall.dto';
+import {
+  CompanyRequestDto,
+  ScheduleListRequestDto,
+  BusSearchRequestDto,
+  BusSeatControlRequestDto,
+  BoardingPointRequestDto,
+  ServiceInformationRequestDto,
+  BusSaleRequestDto,
+  BusRouteRequestDto,
+} from './dto/biletall.dto';
 
 @Controller('biletall')
 export class BiletAllController {
@@ -19,5 +30,35 @@ export class BiletAllController {
   @Post('schedule-list')
   async scheduleList(@Body() requestDto: ScheduleListRequestDto) {
     return this.biletAllService.scheduleList(requestDto);
+  }
+
+  @Post('bus-search')
+  async busSearch(@Body() requestDto: BusSearchRequestDto) {
+    return this.biletAllService.busSearch(requestDto);
+  }
+
+  @Post('bus-seat-control')
+  async busSeatControl(@Body() requestDto: BusSeatControlRequestDto) {
+    return this.biletAllService.busSeatControl(requestDto);
+  }
+
+  @Post('boarding-point')
+  async boardingPoint(@Body() requestDto: BoardingPointRequestDto) {
+    return this.biletAllService.boardingPoint(requestDto);
+  }
+
+  @Post('service-information')
+  async serviceInformation(@Body() requestDto: ServiceInformationRequestDto) {
+    return this.biletAllService.serviceInformation(requestDto);
+  }
+
+  @Post('sale-request')
+  async saleRequest(@Body() requestDto: BusSaleRequestDto) {
+    return this.biletAllService.saleRequest(requestDto);
+  }
+
+  @Post('get-route')
+  async getRoute(@Body() requestDto: BusRouteRequestDto) {
+    return this.biletAllService.getRoute(requestDto);
   }
 }
--- src/tickets/biletall/biletall.service.ts
+++ src/tickets/biletall/biletall.service.ts
@@ -1,42 +1,23 @@
+// src/biletall/biletall.service.ts
+
 import { Injectable } from '@nestjs/common';
 import * as xml2js from 'xml2js';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
-import { CompanyRequestDto, ScheduleListRequestDto } from './dto/biletall.dto';
+import {
+  CompanyRequestDto,
+  ScheduleListRequestDto,
+  BusSearchRequestDto,
+  BusSeatControlRequestDto,
+  BoardingPointRequestDto,
+  ServiceInformationRequestDto,
+  BusSaleRequestDto,
+  BusRouteRequestDto,
+} from './dto/biletall.dto';
 
 @Injectable()
 export class BiletAllService {
   constructor(private configService: ConfigService) {}
-
-  async company(requestDto: CompanyRequestDto): Promise<any> {
-    const firmalarXml = `<Firmalar>${requestDto.FirmaNo}</Firmalar>`;
-    return this.run(firmalarXml);
-  }
-
-  async stopPoints(): Promise<any> {
-    const firmalarXml = `<KaraNoktaGetirKomut/>`;
-    return this.run(firmalarXml);
-  }
-
-  async scheduleList(requestDto: ScheduleListRequestDto): Promise<any> {
-    const builder = new xml2js.Builder({ headless: true });
-    const requestDocument = {
-      Sefer: {
-        FirmaNo: requestDto.FirmaNo,
-        KalkisNoktaID: requestDto.KalkisNoktaID,
-        VarisNoktaID: requestDto.VarisNoktaID,
-        Tarih: requestDto.Tarih,
-        IslemTipi: requestDto.IslemTipi,
-        YolcuSayisi: requestDto.YolcuSayisi,
-        Ip: requestDto.Ip,
-        ...(requestDto.AraNoktaGelsin !== undefined && {
-          AraNoktaGelsin: requestDto.AraNoktaGelsin ? 1 : 0,
-        }),
-      },
-    };
-    const xml = builder.buildObject(requestDocument);
-    return this.run(xml);
-  }
 
   private async run(bodyXml: string): Promise<any> {
     const soapEnvelope = `
@@ -62,8 +43,6 @@ export class BiletAllService {
       },
     };
 
-    //console.log('SOAP Envelope:', soapEnvelope);
-
     try {
       const response = await axios.post(
         this.configService.get<string>('BILETALL_WSDL_URI'), 
@@ -71,12 +50,198 @@ export class BiletAllService {
         config,
       );
 
-      //console.log('Response:', response.data);
       const result = await xml2js.parseStringPromise(response.data);
       return result;
     } catch (error) {
       console.error('Error running XML request:', error);
       throw new Error('Failed to process XML request');
     }
+  }
+
+  async company(requestDto: CompanyRequestDto): Promise<any> {
+    const firmalarXml = `<Firmalar><FirmaNo>${requestDto.FirmaNo}</FirmaNo></Firmalar>`;
+    return this.run(firmalarXml);
+  }
+
+  async stopPoints(): Promise<any> {
+    const stopPointsXml = `<KaraNoktaGetirKomut/>`;
+    return this.run(stopPointsXml);
+  }
+
+  async scheduleList(requestDto: ScheduleListRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      Sefer: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        VarisNoktaID: requestDto.VarisNoktaID,
+        Tarih: requestDto.Tarih,
+        IslemTipi: requestDto.IslemTipi,
+        YolcuSayisi: requestDto.YolcuSayisi,
+        Ip: requestDto.Ip,
+        ...(requestDto.AraNoktaGelsin !== undefined && {
+          AraNoktaGelsin: requestDto.AraNoktaGelsin ? 1 : 0,
+        }),
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async busSearch(requestDto: BusSearchRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      Otobus: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        VarisNoktaID: requestDto.VarisNoktaID,
+        Tarih: requestDto.Tarih,
+        Saat: requestDto.Saat,
+        HatNo: requestDto.HatNo,
+        IslemTipi: requestDto.IslemTipi,
+        YolcuSayisi: requestDto.YolcuSayisi,
+        SeferTakipNo: requestDto.SeferTakipNo,
+        Ip: requestDto.Ip,
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async busSeatControl(requestDto: BusSeatControlRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      OtobusKoltukKontrol: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        VarisNoktaID: requestDto.VarisNoktaID,
+        Tarih: requestDto.Tarih,
+        Saat: requestDto.Saat,
+        HatNo: requestDto.HatNo,
+        IslemTipi: requestDto.IslemTipi,
+        SeferTakipNo: requestDto.SeferTakipNo,
+        Ip: requestDto.Ip,
+        Koltuklar: {
+          Koltuk: requestDto.Koltuklar.map(koltuk => ({
+            KoltukNo: koltuk.KoltukNo,
+            Cinsiyet: koltuk.Cinsiyet,
+          })),
+        },
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async boardingPoint(requestDto: BoardingPointRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      BinecegiYer: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        YerelSaat: requestDto.YerelSaat,
+        HatNo: requestDto.HatNo,
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async serviceInformation(requestDto: ServiceInformationRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      Servis_2: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        YerelSaat: requestDto.YerelSaat,
+        HatNo: requestDto.HatNo,
+        Tarih: new Date(requestDto.Tarih).toISOString(),
+        Saat: new Date(requestDto.Saat).toISOString(),
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async saleRequest(requestDto: BusSaleRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      IslemSatis: {
+        FirmaNo: requestDto.FirmaNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        VarisNoktaID: requestDto.VarisNoktaID,
+        Tarih: new Date(requestDto.Tarih).toISOString(),
+        Saat: new Date(requestDto.Saat).toISOString(),
+        HatNo: requestDto.HatNo,
+        SeferNo: requestDto.SeferNo,
+        TelefonNo: requestDto.TelefonNo,
+        Cinsiyet: requestDto.Cinsiyet,
+        ToplamBiletFiyati: requestDto.ToplamBiletFiyati,
+        YolcuSayisi: requestDto.Passengers.length,
+        BiletSeriNo: requestDto.BiletSeriNo,
+        OdemeSekli: requestDto.OdemeSekli,
+        FirmaAciklama: requestDto.FirmaAciklama,
+        HatirlaticiNot: requestDto.HatirlaticiNot,
+        SeyahatTipi: requestDto.SeyahatTipi,
+        ...requestDto.Passengers.reduce((acc, passenger, index) => {
+          acc[`KoltukNo${index + 1}`] = passenger.KoltukNo;
+          acc[`Adi${index + 1}`] = passenger.Adi;
+          acc[`Soyadi${index + 1}`] = passenger.Soyadi;
+          acc[`Cinsiyet${index + 1}`] = passenger.Cinsiyet;
+          acc[`TcVatandasiMi${index + 1}`] = passenger.TcVatandasiMi;
+          acc[`TcKimlikNo${index + 1}`] = passenger.TcKimlikNo;
+          acc[`PasaportUlkeKod${index + 1}`] = passenger.PasaportUlkeKod;
+          acc[`PasaportNo${index + 1}`] = passenger.PasaportNo;
+          if (passenger.BinecegiYer) acc[`BinecegiYer${index + 1}`] = passenger.BinecegiYer;
+          if (passenger.ServisYeriKalkis) acc[`ServisYeriKalkis${index + 1}`] = passenger.ServisYeriKalkis;
+          if (passenger.ServisYeriVaris) acc[`ServisYeriVaris${index + 1}`] = passenger.ServisYeriVaris;
+          return acc;
+        }, {}),
+        WebYolcu: {
+          WebUyeNo: requestDto.WebYolcu.WebUyeNo,
+          Ip: requestDto.WebYolcu.Ip,
+          Email: requestDto.WebYolcu.Email,
+          ...(requestDto.WebYolcu.KrediKartNo && {
+            KrediKartNo: requestDto.WebYolcu.KrediKartNo,
+            KrediKartSahip: requestDto.WebYolcu.KrediKartSahip,
+            KrediKartGecerlilikTarihi: requestDto.WebYolcu.KrediKartGecerlilikTarihi,
+            KrediKartCCV2: requestDto.WebYolcu.KrediKartCCV2,
+          }),
+          ...(requestDto.WebYolcu.OnOdemeKullan && {
+            OnOdemeKullan: requestDto.WebYolcu.OnOdemeKullan,
+            OnOdemeTutar: requestDto.WebYolcu.OnOdemeTutar,
+          }),
+          ...(requestDto.WebYolcu.AcikPnrNo && {
+            AcikPnrNo: requestDto.WebYolcu.AcikPnrNo,
+            AcikPnrSoyad: requestDto.WebYolcu.AcikPnrSoyad,
+          }),
+          ...(requestDto.WebYolcu.AcikTutar && {
+            AcikTutar: requestDto.WebYolcu.AcikTutar,
+          }),
+          ...(requestDto.WebYolcu.RezervePnrNo && {
+            RezervePnrNo: requestDto.WebYolcu.RezervePnrNo,
+          }),
+        },
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
+  }
+
+  async getRoute(requestDto: BusRouteRequestDto): Promise<any> {
+    const builder = new xml2js.Builder({ headless: true });
+    const requestDocument = {
+      Hat: {
+        FirmaNo: requestDto.FirmaNo,
+        HatNo: requestDto.HatNo,
+        KalkisNoktaID: requestDto.KalkisNoktaID,
+        VarisNoktaID: requestDto.VarisNoktaID,
+        BilgiIslemAdi: requestDto.BilgiIslemAdi,
+        SeferTakipNo: requestDto.SeferTakipNo,
+        Tarih: requestDto.Tarih,
+      },
+    };
+    const xml = builder.buildObject(requestDocument);
+    return this.run(xml);
   }
 }
