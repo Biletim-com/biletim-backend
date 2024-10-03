08d3e04e729e8b9e191d765b1786de974d0abb4c
emre.yilmaz <emre.yilmaz@westerops.com>
2024-10-03 16:13:42 +0300

    fix:arrangement bus schedule list parser

--- src/modules/tickets/bus/services/biletall/biletall-bus-parser.service.ts
+++ src/modules/tickets/bus/services/biletall/biletall-bus-parser.service.ts
@@ -112,11 +112,25 @@ export class BiletAllBusParserService extends BiletAllParserService {
       for (const [key, [value]] of ObjectTyped.entries(entry)) {
         featureParsed[key] = value;
       }
-
       return new BusFeaturesDto(featureParsed);
     });
 
-    return new BusScheduleAndBusFeaturesDto(schedules, features);
+    const combinedSchedulesAndFeatures = schedules.map((schedule) => {
+      const featureIndexes = schedule.busTypeFeature
+        ? schedule.busTypeFeature
+            .split('')
+            .map((value, index) => (value === '1' ? index : -1))
+            .filter((index) => index !== -1)
+        : [];
+      const relatedFeatures = featureIndexes.map((index) => features[index]);
+
+      return {
+        schedule,
+        features: relatedFeatures,
+      };
+    });
+
+    return new BusScheduleAndBusFeaturesDto(combinedSchedulesAndFeatures);
   };
 
   public parseBusSearchResponse = (response: BusResponse): BusSearchDto => {
