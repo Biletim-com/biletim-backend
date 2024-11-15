c918425845ea2a29c3fafe717891cf2418ad3769
emre.yilmaz <emre.yilmaz@westerops.com>
2024-11-15 11:01:53 +0300

    changed location third part provider

--- src/modules/insurance/insurance.module.ts
+++ src/modules/insurance/insurance.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { InsuranceController } from './insurance.controller';
 import { InsuranceService } from './services/insurance.service';
-import { TamamliyoModule } from './tamamliyo/tamamliyo.module';
+import { TamamliyoModule } from './providers/tamamliyo/tamamliyo.module';
 
 @Module({
   imports: [InsuranceModule, TamamliyoModule],
