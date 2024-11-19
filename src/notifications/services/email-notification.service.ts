b5764ba4660b7ab654961f2bb8a207aebd793ef1
Selver Said <selver.said01@gmail.com>
2024-11-20 02:54:16 +0300

    added config for queue

--- src/notifications/services/email-notification.service.ts
+++ src/notifications/services/email-notification.service.ts
@@ -5,27 +5,40 @@ import { OnEvent, OnEvents } from '@app/providers/event-emitter/decorators';
 
 import { EmailNotificationStrategy } from '../strategies/email-notification.strategy';
 import { AuthConfigService } from '@app/configs/auth';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
 
 @Injectable()
 export class EmailNotificationService {
   constructor(
     private readonly notification: EmailNotificationStrategy,
     private readonly authConfigService: AuthConfigService,
-  ) {}
+    @InjectQueue('queue') private readonly queue: Queue
+  ) { }
 
   @OnEvents(['user.created', 'user.email.updated'])
   async sendSetupProfileEmail({
     recipient,
     verificationCode,
   }: SendVerifyAccountEmailNotificationDto): Promise<void> {
-    this.notification.send(recipient, 'Email Verification', {
-      template: 'verify-email',
-      context: {
-        header: 'Email Verification',
-        content: 'Verification code',
-        verificationCode,
-      },
+    await this.queue.add('verifyEmail', {
+      recipient: recipient, subject: 'Email Verification', options: {
+        template: 'verify-email',
+        context: {
+          header: 'Email Verification',
+          content: 'Verification code',
+          verificationCode,
+        },
+      }
     });
+    // await this.notification.send(recipient, 'Email Verification', {
+    //   template: 'verify-email',
+    //   context: {
+    //     header: 'Email Verification',
+    //     content: 'Verification code',
+    //     verificationCode,
+    //   },
+    // });
   }
 
   @OnEvent('user.password.reset')
@@ -33,13 +46,21 @@ export class EmailNotificationService {
     recipient,
     forgotPasswordCode,
   }: SendVerifyAccountEmailNotificationDto): Promise<void> {
-    this.notification.send(recipient, 'Password reset', {
+    await this.queue.add('resetPassword', {recipient: recipient, subject:'Password reset', options: {
       template: 'reset-password',
       context: {
         header: 'Password reset',
         content: 'Click the button below to reset your password.',
         url: `${this.authConfigService.resetPasswordUrl}?verificationCode=${forgotPasswordCode}`,
       },
-    });
+    }});
+    // await this.notification.send(recipient, 'Password reset', {
+    //   template: 'reset-password',
+    //   context: {
+    //     header: 'Password reset',
+    //     content: 'Click the button below to reset your password.',
+    //     url: `${this.authConfigService.resetPasswordUrl}?verificationCode=${forgotPasswordCode}`,
+    //   },
+    // });
   }
 }
--- src/providers/queue/provider.module.ts
+++ src/providers/queue/provider.module.ts
@@ -1,15 +1,19 @@
 import { Module, Global } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { QueueProviderService } from './provider.service';
+import { QueueConfigService } from '@app/configs/queue'; 
 
 @Global()
 @Module({
   imports: [
     BullModule.forRootAsync({
       useClass: QueueProviderService,
-      inject: [QueueProviderService],
+    }),
+    BullModule.registerQueue({
+      name: 'queue',
     }),
   ],
+  providers: [QueueProviderService, QueueConfigService],
 })
 
 export class QueueProviderModule {}
